dd2348a5d46906504ff74de235a5a3c67e43b960
niklasribbon <niklas@ribbonapp.com>
2022-10-18 10:49:38 +0100

    Updated API calls

--- SwiftUI/RibbonSwiftUISample/ContentView.swift
+++ SwiftUI/RibbonSwiftUISample/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import RibbonKit
+import Combine
 
 struct ContentView: View {
     var body: some View {
@@ -17,7 +18,43 @@ struct ContentView: View {
             Text("Hello, Ribbon!")
         }
         .onAppear {
-            Ribbon.shared.trigger("NAME_OF_YOUR_TRIGGER")
+            // These are the different way the API can be used.
+            // 1 Trigger and show attached to a new overlayed window managed by Ribbon
+            Ribbon.shared.trigger("YOUR_TRIGGER")
+
+            // 2 Tigger with a callback state. Then manually show it. Optionally pass your own view controller, if nil, it will attach to new window.
+//            Ribbon.shared.trigger("YOUR_TRIGGER") { state in
+//                switch state {
+//                case .ready:
+//                    Ribbon.shared.show()
+////                    Ribbon.shared.show(from: UIApplication.shared.rootViewController!)
+//                case .noStudy:
+//                    break
+//                case .error(let error):
+//                    print(error.localizedDescription)
+//                @unknown default:
+//                    break
+//                }
+//            }
+
+            // 3 Same as 2 above, but with await async and throwing error. Wrap in a Task, or use .task for iOS15+
+//            Task.init() {
+//                do {
+//                    let state = try await Ribbon.shared.triggerThrowing("YOUR_TRIGGER")
+//                    switch state {
+//                    case .ready:
+//                        Ribbon.shared.show()
+//                    case .noStudy:
+//                        break
+//                    @unknown default:
+//                        break
+//                    }
+//                }
+//                catch {
+//                    print(error.localizedDescription)
+//                }
+//            }
+            
         }
         .padding()
     }
@@ -27,4 +64,23 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+extension UIApplication {
+  var currentKeyWindow: UIWindow? {
+    UIApplication.shared.connectedScenes
+      .filter { $0.activationState == .foregroundActive }
+      .map { $0 as? UIWindowScene }
+      .compactMap { $0 }
+      .first?.windows
+      .filter { $0.isKeyWindow }
+      .first
+  }
+
+  var rootViewController: UIViewController? {
+    currentKeyWindow?.rootViewController
+  }
+
+
 }
--- UIKit/RibbonUIKitSample/ViewController.swift
+++ UIKit/RibbonUIKitSample/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RibbonKit
+import Combine
 
 class ViewController: UIViewController {
 
@@ -15,11 +16,66 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        Ribbon.shared.trigger("NAME_OF_YOUR_TRIGGER")
+        // These are the different way the API can be used.
+        // 1 Trigger and show attached to a new overlayed window managed by Ribbon
+        Ribbon.shared.trigger("YOUR_TRIGGER")
+
+        // 2 Tigger with a callback state. Then manually show it. Optionally pass your own view controller, if nil, it will attach to new window.
+//        Ribbon.shared.trigger("YOUR_TRIGGER") { [weak self] state in
+//            guard let self else { return }
+//                switch state {
+//                case .ready:
+////                    Ribbon.shared.show()
+//                    Ribbon.shared.show(from: self)
+//                case .noStudy:
+//                    break
+//                case .error(let error):
+//                    print(error.localizedDescription)
+//                @unknown default:
+//                    break
+//                }
+//            }
+
+        // 3 Same as 2 above, but with await async and throwing error. Wrap in a Task, or use .task for iOS15+
+//            Task.init() {
+//                do {
+//                    let state = try await Ribbon.shared.triggerThrowing("YOUR_TRIGGER")
+//                    switch state {
+//                    case .ready:
+//                        Ribbon.shared.show()
+//                    case .noStudy:
+//                        break
+//                    @unknown default:
+//                        break
+//                    }
+//                }
+//                catch {
+//                    print(error.localizedDescription)
+//                }
+//            }
     }
 
 
 }
 
+
+extension UIApplication {
+  var currentKeyWindow: UIWindow? {
+    UIApplication.shared.connectedScenes
+      .filter { $0.activationState == .foregroundActive }
+      .map { $0 as? UIWindowScene }
+      .compactMap { $0 }
+      .first?.windows
+      .filter { $0.isKeyWindow }
+      .first
+  }
+
+  var rootViewController: UIViewController? {
+    currentKeyWindow?.rootViewController
+  }
+
+
+}
