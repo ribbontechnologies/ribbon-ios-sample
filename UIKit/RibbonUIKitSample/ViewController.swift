1c7c4d55576b433c23b1f8b142a323e041e0846e
julienribbon <124575395+julienribbon@users.noreply.github.com>
2023-02-09 13:49:51 +0000

    Updating UIKit code sample for SDK version 1.1.0

--- UIKit/RibbonUIKitSample/ViewController.swift
+++ UIKit/RibbonUIKitSample/ViewController.swift
@@ -20,45 +20,40 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         // These are the different way the API can be used.
-        // 1 Trigger and show attached to a new overlayed window managed by Ribbon
-        Ribbon.shared.trigger("YOUR_TRIGGER")
-
-        // 2 Tigger with a callback state. Then manually show it. Optionally pass your own view controller, if nil, it will attach to new window.
-//        Ribbon.shared.trigger("YOUR_TRIGGER") { [weak self] state in
-//            guard let self else { return }
+        
+        // 1 Tigger with a callback state. Then manually show it.
+        Ribbon.shared.trigger("YOUR_TRIGGER") { [weak self] state in
+            guard let self else { return }
+                switch state {
+                case .ready:
+                    Ribbon.shared.show(from: self)
+                case .noStudy:
+                    break
+                case .error(let error):
+                    print(error.localizedDescription)
+                @unknown default:
+                    break
+                }
+            }
+        
+        // 2 Same as 1 above, but with await async and throwing error.
+//        Task.init() {
+//            do {
+//                let state = try await Ribbon.shared.trigger("YOUR_TRIGGER")
 //                switch state {
 //                case .ready:
-////                    Ribbon.shared.show()
 //                    Ribbon.shared.show(from: self)
 //                case .noStudy:
 //                    break
-//                case .error(let error):
-//                    print(error.localizedDescription)
 //                @unknown default:
 //                    break
 //                }
 //            }
-
-        // 3 Same as 2 above, but with await async and throwing error. Wrap in a Task, or use .task for iOS15+
-//            Task.init() {
-//                do {
-//                    let state = try await Ribbon.shared.triggerThrowing("YOUR_TRIGGER")
-//                    switch state {
-//                    case .ready:
-//                        Ribbon.shared.show()
-//                    case .noStudy:
-//                        break
-//                    @unknown default:
-//                        break
-//                    }
-//                }
-//                catch {
-//                    print(error.localizedDescription)
-//                }
+//            catch {
+//                print(error.localizedDescription)
 //            }
+//        }
     }
-
-
 }
 
 
